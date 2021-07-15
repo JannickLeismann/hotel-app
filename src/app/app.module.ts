0c8521f1e03dda38c844a2bbbce89c77285783b7
JannickL <jannick.leismann@gmx.de>
2021-07-15 14:13:35 +0200

    Bookings Component hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookingsComponent } from './bookings/bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent implements OnInit {
+
+  constructor() { }
+
+  booking = "Jannicks Buchung";
+
+  ngOnInit(): void {
+  }
+
+}
