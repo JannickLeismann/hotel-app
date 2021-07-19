5c959438de8c4405d4776623265909a4f2bec184
JannickL <jannick.leismann@gmx.de>
2021-07-19 15:40:07 +0200

    Booking Service implementieren Teil 2

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -17,4 +17,18 @@ export class BookingService {
     var index = Bookings.indexOf(booking);
     Bookings.splice(index,1);
   }
+
+  getBookingById(id: number) : Booking {
+    var bookingById = Bookings.find(x => x.id == id)!;
+    return bookingById;
+  }
+
+  addBooking(booking: Booking) : void {
+    Bookings.push(booking);
+  }
+
+  updateBooking(booking: Booking) : void {
+    var currentBooking = this.getBookingById(booking.id);
+    currentBooking = booking;
+  }
 }
--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Booking } from '../booking';
 import { Router, ActivatedRoute } from '@angular/router';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-create-booking',
@@ -9,7 +10,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CreateBookingComponent implements OnInit {
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(
+     private router: Router,
+     private activatedRoute: ActivatedRoute,
+     private bookingService:BookingService) { }
 
   booking: Booking = {
     id: 100,
@@ -22,18 +26,18 @@ export class CreateBookingComponent implements OnInit {
   ngOnInit(): void {   
     if(this.router.url != '/create'){
       var id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
-      var bookingById = Bookings.find(x => x.id == id)!;
+      var bookingById = this.bookingService.getBookingById(id);
       this.booking = bookingById;
     }    
   }
 
   save(): void {
-    var bookingById = Bookings.find(x => x.id == this.booking.id);
+    var bookingById = this.bookingService.getBookingById(this.booking.id);
 
     if(bookingById == null || bookingById == undefined){
-      Bookings.push(this.booking);
+      this.bookingService.addBooking(this.booking);
     } else {
-      bookingById = this.booking;
+      this.bookingService.updateBooking(this.booking);
     }
 
     this.router.navigate(['bookings']);
