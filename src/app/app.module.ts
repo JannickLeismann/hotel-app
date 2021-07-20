8a5ea20799fad353182d0801ba99eff9f162e852
JannickL <jannick.leismann@gmx.de>
2021-07-20 11:16:08 +0200

    InMemoryWebAPI konfiguriert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BookingsComponent } from './bookings/bookings.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CreateBookingComponent } from './create-booking/create-booking.component';
-
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Booking } from './booking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService  {
+
+  createDb() {
+    const bookings: Booking[] = [
+      {
+          id : 1,
+          name: "Jannick Leismann",
+          roomNumber: 100,
+          startDate: new Date(),
+          endDate: new Date("2021-07-21")
+      },
+      {
+          id : 2,
+          name: "Max Mustermann",
+          roomNumber: 50,
+          startDate: new Date("2021-07-25"),
+          endDate: new Date("2021-08-01")
+      },
+      {
+          id : 3,
+          name: "John Doe",
+          roomNumber: 2,
+          startDate: new Date("2021-09-05"),
+          endDate: new Date("2021-09-19")
+      },
+      {
+          id : 4,
+          name: "Maria Doemann",
+          roomNumber: 105,
+          startDate: new Date("2021-08-04"),
+          endDate: new Date("2021-08-11")
+      }
+    ]
+
+    return bookings;
+  }
+
+  constructor() { }
+}
